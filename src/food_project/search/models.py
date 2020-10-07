50074e37adb71c9f3d4421fab8c8dabf502d9619
Sherin John <sherinjohn625@gmail.com>
2020-10-07 15:34:16 +0530

    Added Models.

--- src/food_project/search/models.py
+++ src/food_project/search/models.py
@@ -4,3 +4,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=4)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
